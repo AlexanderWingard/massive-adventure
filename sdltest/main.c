1c50f01a1f6de2e73b8ea715a723b54bc441dae0
Alexander Wingård <alexander.wingard@gmail.com>
2012-11-13 23:10:26 +0100

    Initial Commit

---
+++ sdltest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sdltest
+//
+//  Created by Alexander Wingård on 2012-11-13.
+//  Copyright (c) 2012 Alexander Wingård. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
